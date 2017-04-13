d5d4cee172d8e754c63b09bdf71b3af99182d1f4
Brady Ericksen <bradyericksen@gmail.com>
2017-04-12 19:22:37 -0600

    Added sectionize app, needs to be tested

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ let cheerio = require('cheerio');
 
 let thHundredBooksUrl = 'http://thehundredbooks.com/';
 
+const pageSize = 500;
+
+//debugger;
+
 function getBookContentsForAllBooks(hrefForAllBooks) {
     let books = [];
     let countAdded = 0;
@@ -57,6 +61,7 @@ function saveBooks(books) {
     books.forEach(book => {
         console.log(`Title: ${book.title}`);
         console.log(book.contents);
+        sectionize(book);
     });
 }
 
@@ -75,6 +80,45 @@ function getHrefForAllBooks($) {
 
 function emptyTest() {
 
+}
+
+function sectionize(book) {
+    let content = book.contents;
+    let totalChars = content.length;
+    let lastIndex = 0;
+    let cleanBreak = false;
+    let pageIncrement = pageSize;
+    //alert("In sectionize");
+    //alert(totalChars);
+    //let pages = Math.ceil(totalChars/pageSize); //divide char count by max chars per page to find out number of pages
+    //alert(pages);
+    //var pages = bookText.match(/.{1,300}/g);
+    for(i = 0; i < totalChars; i+= pageIncrement)
+    {
+        console.log("Page " + (i + 1));
+        lastIndex = i + pageSize;
+        let pageEndChar = content.charAt(lastIndex) //find last char on page
+        if(pageEndChar != "" && pageEndChar != " " && /[^a-zA-Z0-9]/.test(pageEndChar) == true) //see if it splits cleanly by miraculous chance
+        {
+            for(j = lastIndex; cleanBreak == false; j--)
+            {
+                lastIndex = j - 1;
+                pageEndChar = content.charAt(lastIndex);
+                if(pageEndChar == "" || pageEndChar == " " || /[^a-zA-Z0-9]/.test(pageEndChar) == false){ //if it meets the condition for page break
+                    cleanBreak = true; //then break out
+                }
+            }
+        }
+        let pageContent = content.substring(i, i + lastIndex + 1);
+        console.log(pageContent);
+        console.log("\r\n");
+        //document.getElementById("pages").innerHTML += pageContent + "_____________________________";
+        pageIncrement = lastIndex; //i + last index?
+        cleanBreak = false;
+        lastIndex = 0;
+    }
+    //document.getElementById("pagesDiv").innerHTML = pages;
+    //pages.split(',');
 }
 
 scrapeWebPage();
