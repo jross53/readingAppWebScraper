64d0d97dbf2997b52f4cb5a3c448d19c6cae9f2b
Jordan.Ross <jord_ross@hotmail.com>
2017-04-22 17:39:57 -0600

    Added a bootstrap modal

--- web/BookController.js
+++ web/BookController.js
@@ -1,49 +1,37 @@
 /**
  * Created by Jordan.Ross on 4/18/2017.
  */
-let readingApp = angular.module('readingApp', []); //'ui.bootstrap.modal'
+let readingApp = angular.module('readingApp', []);
 
-readingApp.controller('BookController', ['$scope', '$http', function($scope, $http) {
-    $scope.books = [];
+readingApp.controller('BookController', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.books = [];
+        $scope.modelIsHidden = true;
 
-    function getBooks() {
-        $http.get('/books').then(displayBooks);
+        function getBooks() {
+            $http.get('/books').then(displayBooks);
+        }
+
+        function displayBooks(books) {
+            $scope.books = books.data;
+        }
+
+        $scope.getPercentage = function (book) {
+            return Math.floor(book.currentPage / book.totalPages);
+        };
+
+        $scope.displayBook = function (book) {
+            $scope.modelIsHidden = false;
+            console.log(`Book clicked: ${book.title}`);
+            $scope.selectedBook = book;
+            $scope.currentPageNumber = book.currentPage;
+            $scope.currentPage = book.pages[$scope.currentPageNumber - 1];
+        };
+
+        $scope.close = function (result) {
+            $scope.modelIsHidden = true;
+        };
+
+        getBooks();
     }
-
-    function displayBooks(books) {
-        // for(let index = 0; index < 10; index++) {
-        //     books.data[index].percentageRead = 100;
-        // }
-        //
-        // for(let index = 10; index < books.data.length; index++) {
-        //     books.data[index].percentageRead = Math.floor(Math.random() * 101);
-        // }
-
-        $scope.books = books.data;
-    }
-
-    $scope.getPercentage = function(book) {
-        console.log("Book current page: " + book.currentPage);
-        console.log("Book total pages: " + book.totalPages);
-        console.log(book.title + "percentage is " + book.currentPage / book.totalPages);
-        return Math.floor(book.currentPage / book.totalPages);
-    };
-
-    $scope.displayBook = function(book) {
-        console.log(`Book clicked: ${book.title}`);
-        $scope.exampleModal = true;
-    };
-    // $scope.open = function() {
-    //     $scope.exampleModal = true;
-    // };
-
-    $scope.ok = function() {
-        $scope.exampleModal = false;
-    };
-
-    $scope.cancel = function() {
-        $scope.exampleModal = false;
-    };
-
-    getBooks();
-}]);
+]);
